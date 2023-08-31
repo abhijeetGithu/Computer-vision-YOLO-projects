37df001f00599ca1af32bffa875261b9a26e3714
abhijeetGithu <abhijeetas8660211@gmail.com>
2023-08-31 23:49:19 +0530

    Create PPEdetection.py

---
+++ Projects/PPEdetection/PPEdetection.py
@@ -0,0 +1,57 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import time
+
+# cap = cv2.VideoCapture(0)  # For Webcam
+# cap.set(3, 1280)
+# cap.set(4, 720)
+cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+
+
+model = YOLO("best.pt") # you have to add the training data.pt type file here to get the project done
+
+classNames = ['Hardhat', 'Mask', 'NO-Hardhat', 'NO-Mask', 'NO-Safety Vest', 'Person', 'Safety Cone', 'Safety Vest', 'machinery', 'vehicle']
+
+prev_frame_time = 0
+new_frame_time = 0
+myColor = (0, 0, 255)
+while True:
+    new_frame_time = time.time()
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            #cvzone.cornerRect(img, (x1, y1, w, h))
+
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            currentClass=classNames[cls]
+            if conf > 0.5:
+                if currentClass == 'NO-Hardhat' or currentClass == 'NO-Safety Vest' or currentClass == "NO-Mask":
+                    myColor = (0, 0, 255)
+                elif currentClass == 'Hardhat' or currentClass == 'Safety Vest' or currentClass == "Mask" or currentClass == "vehicle":
+                    myColor = (0, 255, 0)
+                else:
+                    myColor = (255, 0, 0)
+
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
+                                   (max(0, x1), max(35, y1)), scale=1, thickness=1, colorB=myColor,
+                                   colorT=(255, 255, 255), colorR=myColor, offset=5)
+                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
+
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    print(fps)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
